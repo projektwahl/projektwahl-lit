342cd07b25e390eff47797708f939bd6667af32f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-15 00:47:28 +0200

    Start working on form and input implementation

---
+++ src/lib/form/pw-input.ts
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+import { html, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { bootstrapCss } from "../..";
+import { HistoryController } from "../../history-controller";
+
+@customElement("pw-input")
+export class PwInput extends LitElement {
+  private history = new HistoryController(this);
+
+  @property({ type: String })
+  label: string;
+
+  @property({ type: String })
+  type: "text" | "password";
+
+  @property({ type: String })
+  name: string;
+
+  @property({ type: String })
+  autocomplete: "username" | "current-password";
+
+  @state()
+  randomId: string;
+
+  constructor() {
+    super();
+    this.randomId = "id" + Math.random().toString().replace(".", "");
+  }
+
+  render() {
+    return html`
+      ${bootstrapCss}
+      <div class="mb-3">
+        <label for=${this.randomId} class="form-label">${this.label}:</label>
+        <input
+          type=${this.type}
+          class="form-control ${"is-invalid"}"
+          name=${this.name}
+          id=${this.randomId}
+          aria-describedby="${this.randomId}-feedback"
+          autocomplete=${this.autocomplete}
+        />
+        <div id="${this.randomId}-feedback" class="invalid-feedback">error</div>
+      </div>
+    `;
+  }
+}
--- src/routes/login/index.ts
+++ src/routes/login/index.ts
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+import "../../lib/form/pw-input"; // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 import { html, LitElement, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
@@ -16,13 +16,51 @@ export class PwLogin extends LitElement {
 
   data: string;
 
+  login = (event: SubmitEvent) => {
+    event.preventDefault();
+  };
+
+  // https://www.chromestatus.com/feature/4708990554472448
+  // https://www.reddit.com/r/PolymerJS/comments/f00gd0/litelement_formassociated_custom_elements_form/
+  // https://css-tricks.com/creating-custom-form-controls-with-elementinternals/
+  // https://web.dev/more-capable-form-controls/
+  // https://caniuse.com/mdn-api_htmlformelement_formdata_event
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/formdata_event
+  // safari our friend ...
+  // https://web.dev/more-capable-form-controls/#feature-detection
+  /*
+You can use feature detection to determine whether the formdata event and form-associated custom elements are available. There are currently no polyfills released for either feature. In both cases, you can fall back to adding a hidden form element to propagate the control's value to the form. Many of the more advanced features of form-associated custom elements will likely be difficult or impossible to polyfill.
+if ('FormDataEvent' in window) {
+  // formdata event is supported
+}
+  */
+
   render() {
     return html`
       ${bootstrapCss}
       <main class="container">
         <h1 class="text-center">Login</h1>
 
-        ${this.data}
+        <div class="row justify-content-center">
+          <div class="col-md-7 col-lg-8">
+            <form method="POST" action="/no-javascript" @submit=${this.login}>
+              <pw-input
+                type="text"
+                autocomplete="username"
+                label="Name"
+                name="name"
+              ></pw-input>
+              <pw-input
+                label="Passwort"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+              ></pw-input>
+
+              <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+          </div>
+        </div>
       </main>
     `;
   }
