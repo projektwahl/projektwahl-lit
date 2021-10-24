dbc95ddd1f6a8f7a985271b98de01e8a4795cfcf
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-24 22:47:02 +0200

    Further progress

--- audit.js
+++ audit.js
@@ -53,12 +53,67 @@ async function gitClonePackage([pkgpath, pkg], package_json, version) {
     }
 }
 
+async function typesPackage([pkgpath, pkg], package_json) {
+    // please tell me how to extract version better out of @types packages
+    // there is a hash in the package.json we could calculate but I don't if that works.
+
+    let repository = package_json.repository
+    if (typeof repository === "object") repository = repository.url;
+    if (/^[a-zA-Z0-9-]+\/[a-zA-Z0-9-\.]+$/.test(repository)) {
+        repository = "https://github.com/"+repository+".git"
+    }
+    repository = repository.replace(/^git\+/, "");
+    console.log(repository)
+    // TODO FIXME parse the URL so we at least have a little bit more safety of malicious inputs
+
+    if (!existsSync(join("/tmp", pkgpath))) {
+        try {
+            let result = await asyncExecFile("git", ["clone", "--filter=tree:0", "--branch", "master", "--no-checkout", repository, pkgpath], {
+                cwd: "/tmp",
+            })
+            console.log(result)
+
+            let readme_md = await readFile(join(pkgpath, "README.md"), {
+                encoding: "utf-8"
+            })
+
+            let re = /Last updated: (.+)/
+            let readme_time = readme_md.match(re)[1]
+            console.log(readme_time)
+            
+            let commit = await asyncExecFile("git", ["rev-list", "-1", `--before="${readme_time}"`, "master"], {
+                cwd: join("/tmp", pkgpath),
+            })
+            console.log(commit)
+
+            result = await asyncExecFile("git", ["sparse-checkout", "init", "--cone"], {
+                cwd: join("/tmp", pkgpath),
+            })
+            console.log(result)
+
+            result = await asyncExecFile("git", ["sparse-checkout", "set", package_json.repository.directory], {
+                cwd: join("/tmp", pkgpath),
+            })
+            console.log(result)
+
+            result = await asyncExecFile("git", ["checkout", commit.stdout.trim()], {
+                cwd: join("/tmp", pkgpath),
+            })
+            console.log(result)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
 for (const [pkgpath, pkg] of Object.entries(package_lock.packages)) {
     if (pkgpath === "") continue;
     // https://docs.npmjs.com/cli/v7/configuring-npm/package-json
     let package_json = JSON.parse(await readFile(join(pkgpath, "package.json")))
 
-    if (pkgpath.endsWith("/lodash.merge")) {
+    if (pkgpath.includes("/@types/")) {
+        await typesPackage([pkgpath, pkg], package_json)
+    } else if (pkgpath.endsWith("/lodash.merge")) {
         await gitClonePackage([pkgpath, pkg], package_json, `${pkg.version}-npm-packages`);
     } else if (pkgpath.endsWith("/lit-html")) {
         await gitClonePackage([pkgpath, pkg], package_json, `lit-html@${pkg.version}`);
