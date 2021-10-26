ab9aa65ca16aed7ca0493d8672ae21493d8844d5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-27 00:37:26 +0200

    savepoint

--- audit.js
+++ audit.js
@@ -1,5 +1,4 @@
-import { readFile, writeFile } from 'fs/promises';
-import { existsSync } from 'fs';
+import { readFile, writeFile, mkdtemp } from 'fs/promises';
 import { join } from 'path';
 import { execFile } from 'child_process'
 import { promisify } from 'util';
@@ -58,118 +57,64 @@ diffoscope old_node_modules/ node_modules/
 // https://docs.npmjs.com/cli/v7/configuring-npm/package-lock-json
 let package_lock = JSON.parse(await readFile("./package-lock.json"));
 
-async function gitClonePackage([pkgpath, pkg], package_json, version) {
-    let repository = package_json.repository
-    if (typeof repository === "object") repository = repository.url;
-    if (/^[a-zA-Z0-9-]+\/[a-zA-Z0-9-\.]+$/.test(repository)) {
-        repository = "https://github.com/"+repository+".git"
-    }
-    repository = repository.replace(/^git\+/, "");
-    repository = repository.replace(/\/tree\/.+/, "");
-
-    console.log(repository)
+async function gitClonePackage([pkgpath, pkg], package_json, commit) {
     // TODO FIXME parse the URL so we at least have a little bit more safety of malicious inputs
+    let directory = await mkdtemp(path.join(os.tmpdir(), 'foo-'));
+    try {
+        let result = await asyncExecFile("git", ["clone", "--filter=tree:0", "--no-checkout", package_json.repository, directory], {
+            cwd: "/tmp",
+        })
+        console.log(result)
 
-    if (!existsSync(join("/tmp", pkgpath))) {
-        try {
-            let { error2, stdout2, stderr2 } = await asyncExecFile("git", ["clone", "--depth", "1", "--branch", version, repository, pkgpath], {
-                cwd: "/tmp",
-            })
-            console.log(stdout2)
-            console.error(stderr2)
-            console.error(error2)
+        result = await asyncExecFile("git", ["checkout", "--depth", "1", commit], {
+            cwd: join("/tmp", directory),
+        })
 
-            // TODO FIXME that this also works with existing directories
-            return `git+${repository}#${version}`
-        } catch (error) {
-            console.error(error)
-            return undefined
-        }
-    }
-}
+        result = await asyncExecFile("git", ["rev-parse", "HEAD"], {
+            cwd: join("/tmp", directory),
+        })
 
-async function gitClonePackageCommit([pkgpath, pkg], package_json, commit) {
-    let repository = package_json.repository
-    if (typeof repository === "object") repository = repository.url;
-    if (/^[a-zA-Z0-9-]+\/[a-zA-Z0-9-\.]+$/.test(repository)) {
-        repository = "https://github.com/"+repository+".git"
-    }
-    repository = repository.replace(/^git\+/, "");
-    //console.log(repository)
-    // TODO FIXME parse the URL so we at least have a little bit more safety of malicious inputs
-
-    if (!existsSync(join("/tmp", pkgpath))) {
-        try {
-            let result = await asyncExecFile("git", ["clone", "--filter=tree:0", "--no-checkout", repository, pkgpath], {
-                cwd: "/tmp",
-            })
-            console.log(result)
-
-            result = await asyncExecFile("git", ["checkout", commit], {
-                cwd: join("/tmp", pkgpath),
-            })
-            console.log(result)
-            return `git+${repository}#${commit}`
-        } catch (error) {
-            console.error(error)
-            return undefined
-        }
+        return result.stdout.trim();
+    } catch (error) {
+        console.error(error)
+        return undefined
     }
 }
 
 async function typesPackage([pkgpath, pkg], package_json) {
-    // please tell me how to extract version better out of @types packages
-    // there is a hash in the package.json we could calculate but I don't if that works.
+    let result = await asyncExecFile("git", ["clone", "--filter=tree:0", "--branch", "master", "--no-checkout", package_json.repository, directory], {
+        cwd: "/tmp",
+    })
+    console.log(result)
 
-    let repository = package_json.repository
-    if (typeof repository === "object") repository = repository.url;
-    if (/^[a-zA-Z0-9-]+\/[a-zA-Z0-9-\.]+$/.test(repository)) {
-        repository = "https://github.com/"+repository+".git"
-    }
-    repository = repository.replace(/^git\+/, "");
-    //console.log(repository)
-    // TODO FIXME parse the URL so we at least have a little bit more safety of malicious inputs
+    let readme_md = await readFile(join(directory, "README.md"), {
+        encoding: "utf-8"
+    })
 
-    if (!existsSync(join("/tmp", pkgpath))) {
-        try {
-            let result = await asyncExecFile("git", ["clone", "--filter=tree:0", "--branch", "master", "--no-checkout", repository, pkgpath], {
-                cwd: "/tmp",
-            })
-            console.log(result)
+    let re = /Last updated: (.+)/
+    let readme_time = readme_md.match(re)[1]
+    console.log(readme_time)
 
-            let readme_md = await readFile(join(pkgpath, "README.md"), {
-                encoding: "utf-8"
-            })
+    let commit = await asyncExecFile("git", ["rev-list", "-1", `--before="${readme_time}"`, "master"], {
+        cwd: join("/tmp", directory),
+    })
+    console.log(commit)
 
-            let re = /Last updated: (.+)/
-            let readme_time = readme_md.match(re)[1]
-            console.log(readme_time)
+    result = await asyncExecFile("git", ["sparse-checkout", "init", "--cone"], {
+        cwd: join("/tmp", directory),
+    })
+    console.log(result)
 
-            let commit = await asyncExecFile("git", ["rev-list", "-1", `--before="${readme_time}"`, "master"], {
-                cwd: join("/tmp", pkgpath),
-            })
-            console.log(commit)
+    result = await asyncExecFile("git", ["sparse-checkout", "set", package_json.repository.directory], {
+        cwd: join("/tmp", directory),
+    })
+    console.log(result)
 
-            result = await asyncExecFile("git", ["sparse-checkout", "init", "--cone"], {
-                cwd: join("/tmp", pkgpath),
-            })
-            console.log(result)
-
-            result = await asyncExecFile("git", ["sparse-checkout", "set", package_json.repository.directory], {
-                cwd: join("/tmp", pkgpath),
-            })
-            console.log(result)
-
-            result = await asyncExecFile("git", ["checkout", commit.stdout.trim()], {
-                cwd: join("/tmp", pkgpath),
-            })
-            console.log(result)
-            return `git+${repository}#${commit.stdout.trim()}`
-        } catch (error) {
-            console.error(error)
-            return undefined
-        }
-    }
+    result = await asyncExecFile("git", ["checkout", commit.stdout.trim()], {
+        cwd: join("/tmp", directory),
+    })
+    console.log(result)
+    return `git+${repository}#${commit.stdout.trim()}`
 }
 
 for (const [pkgpath, pkg] of Object.entries(package_lock.packages)) {
@@ -182,6 +127,16 @@ for (const [pkgpath, pkg] of Object.entries(package_lock.packages)) {
 
     console.log(package_json.name)
     console.log(package_json.version)
+
+    package_json.repository = package_json.repository
+    if (typeof package_json.repository === "object") package_json.repository = package_json.repository.url;
+    if (/^[a-zA-Z0-9-]+\/[a-zA-Z0-9-\.]+$/.test(package_json.repository)) {
+        package_json.repository = "https://github.com/"+package_json.repository+".git"
+    }
+    package_json.repository = package_json.repository.replace(/^git\+/, "");
+    package_json.repository = package_json.repository.replace(/\/tree\/.+/, "");
+
+    console.log(package_json.repository)
 
     let correct_url;
     if (pkgpath.includes("/@types/")) {
@@ -236,33 +191,33 @@ for (const [pkgpath, pkg] of Object.entries(package_lock.packages)) {
     
     
     else if (package_json.name === "uri-js" && package_json.version === "4.4.1") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "9a328873a21262651c3790505b24c9e318a0e12d")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "9a328873a21262651c3790505b24c9e318a0e12d")
     } else if (package_json.name === "string_decoder" && package_json.version === "0.10.31") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "06bb4afbf163c9e1acd14125618784f9513f39d9")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "06bb4afbf163c9e1acd14125618784f9513f39d9")
     } else if (package_json.name === "pstree.remy" && package_json.version === "1.1.8") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "f82ab879fc8929a5be76828545a0674af14f8d43")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "f82ab879fc8929a5be76828545a0674af14f8d43")
     } else if (package_json.name === "progress" && package_json.version === "2.0.3") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "0790207ef077cbfb7ebde24a1dd9895ebf4643e1")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "0790207ef077cbfb7ebde24a1dd9895ebf4643e1")
     } else if (package_json.name === "path-to-regexp" && package_json.version === "0.1.7") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "4c5412af6fae141f48c32e535bc931573ade99c4")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "4c5412af6fae141f48c32e535bc931573ade99c4")
     } else if (package_json.name === "normalize-url" && package_json.version === "4.5.1") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "454970b662086e8856d1af074c7a57df96545b8b")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "454970b662086e8856d1af074c7a57df96545b8b")
     } else if (package_json.name === "mkdirp" && package_json.version === "0.5.5") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "f2003bbcffa80f8c9744579fabab1212fc84545a")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "f2003bbcffa80f8c9744579fabab1212fc84545a")
     } else if (package_json.name === "json-buffer" && package_json.version === "3.0.0") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "da6fe4c61fd9a5e7b450aecb079219794733b245")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "da6fe4c61fd9a5e7b450aecb079219794733b245")
     } else if (package_json.name === "functional-red-black-tree" && package_json.version === "1.0.1") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "e7c9899a68797f8e891220b4c1a70456991a32a5")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "e7c9899a68797f8e891220b4c1a70456991a32a5")
     } else if (package_json.name === "fill-range" && package_json.version === "7.0.1") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "39f421b499d5c97b62e955c179fa34c062aab2a5")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "39f421b499d5c97b62e955c179fa34c062aab2a5")
     } else if (package_json.name === "@open-wc/dev-server-hmr" && package_json.version === "0.1.2") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "@open-wc/dev-server-hmr@0.1.2-next.0")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "@open-wc/dev-server-hmr@0.1.2-next.0")
     } else if (package_json.name === "file-entry-cache" && package_json.version === "6.0.1") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "c227beb3f0dacc1a8dd95504deabb74a7618c003")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "c227beb3f0dacc1a8dd95504deabb74a7618c003")
     } else if (package_json.name === "acorn-jsx" && package_json.version === "5.3.2") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "f5c107b85872230d5016dbb97d71788575cda9c3")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "f5c107b85872230d5016dbb97d71788575cda9c3")
     }  else if (package_json.name === "@swc/helpers" && package_json.version === "0.2.13") {
-        correct_url = await gitClonePackageCommit([pkgpath, pkg], package_json, "a1a348372ff2c2d1941bbd5eb944de9f6da26628")
+        correct_url = await gitClonePackage([pkgpath, pkg], package_json, "a1a348372ff2c2d1941bbd5eb944de9f6da26628")
     } else {
         correct_url = await gitClonePackage([pkgpath, pkg], package_json, pkg.version);
         if (correct_url === undefined) {
