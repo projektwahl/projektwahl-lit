72b92794f93fa5bfc69ae0a7c650443c11082054
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-28 22:59:22 +0200

    Jo

---
+++ packages/yarn-plugin-dependencies-from-source/sources/MyCustomResolver.ts
@@ -0,0 +1,84 @@
+import {Resolver, ResolveOptions, MinimalResolveOptions} from '@yarnpkg/core';
+import {Descriptor, Locator, Manifest}                   from '@yarnpkg/core';
+import {LinkType}                                        from '@yarnpkg/core';
+import {miscUtils, structUtils}                          from '@yarnpkg/core';
+
+
+export class MyCustomResolver implements Resolver {
+  supportsDescriptor(descriptor: Descriptor, opts: MinimalResolveOptions) {
+    console.log("descriptor: " + descriptor + " opts: " + opts)
+    /*if (!TARBALL_REGEXP.test(descriptor.range))
+      return false;
+
+    if (PROTOCOL_REGEXP.test(descriptor.range))
+      return true;
+
+    return false;*/
+    return false;
+  }
+
+  supportsLocator(locator: Locator, opts: MinimalResolveOptions) {
+    console.log("locator: " + locator + " opts: " + opts)
+
+
+    return false;
+      /*
+    if (!TARBALL_REGEXP.test(locator.reference))
+      return false;
+
+    if (PROTOCOL_REGEXP.test(locator.reference))
+      return true;
+
+    return false;*/
+  }
+
+  shouldPersistResolution(locator: Locator, opts: MinimalResolveOptions) {
+    return true;
+  }
+
+  bindDescriptor(descriptor: Descriptor, fromLocator: Locator, opts: MinimalResolveOptions) {
+    return descriptor;
+  }
+
+  getResolutionDependencies(descriptor: Descriptor, opts: MinimalResolveOptions) {
+    return [];
+  }
+
+  async getCandidates(descriptor: Descriptor, dependencies: unknown, opts: ResolveOptions) {
+    return [structUtils.convertDescriptorToLocator(descriptor)];
+  }
+
+  async getSatisfying(descriptor: Descriptor, references: Array<string>, opts: ResolveOptions) {
+    return null;
+  }
+
+  async resolve(locator: Locator, opts: ResolveOptions) {
+    if (!opts.fetchOptions)
+      throw new Error(`Assertion failed: This resolver cannot be used unless a fetcher is configured`);
+
+    const packageFetch = await opts.fetchOptions.fetcher.fetch(locator, opts.fetchOptions);
+
+    const manifest = await miscUtils.releaseAfterUseAsync(async () => {
+      return await Manifest.find(packageFetch.prefixPath, {baseFs: packageFetch.packageFs});
+    }, packageFetch.releaseFs);
+
+    return {
+      ...locator,
+
+      version: manifest.version || `0.0.0`,
+
+      languageName: manifest.languageName || opts.project.configuration.get(`defaultLanguageName`),
+      linkType: LinkType.HARD,
+
+      conditions: manifest.getConditions(),
+
+      dependencies: manifest.dependencies,
+      peerDependencies: manifest.peerDependencies,
+
+      dependenciesMeta: manifest.dependenciesMeta,
+      peerDependenciesMeta: manifest.peerDependenciesMeta,
+
+      bin: manifest.bin,
+    };
+  }
+}
