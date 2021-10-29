1f42f91c9c051e6217bcb415bfcdd4ce552954b5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-30 01:37:22 +0200

    convert to jsdoc

--- packages/projektwahl-lit/src/lib/result.js
+++ packages/projektwahl-lit/src/lib/result.js
@@ -1,123 +1,94 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 
-import { ZodError } from "zod";
+import { z, ZodError } from "zod";
 
 /** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").OptionalResult<T, E>) => result is import("./types").FailureResult<T, E>} */
 export const isErr = (result) => {
   return result.result === "failure";
 };
 
-export const isOk = <
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(
-  result: OptionalResult<T, E>
-): result is SuccessResult<T, E> => {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").OptionalResult<T, E>) => result is import("./types").SuccessResult<T, E>} */
+export const isOk = (result) => {
   return result.result === "success";
 };
 
-export const ok = <
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(
-  value: T
-): SuccessResult<T, E> => {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(value: T) => import("./types").SuccessResult<T, E>} */
+export const ok = (value) => {
   return {
     result: "success",
     success: value,
   };
 };
 
-export const err = <
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(
-  error: E
-): FailureResult<T, E> => {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(error: E) => import("./types").FailureResult<T, E>} */
+export const err = (error) => {
   return {
     result: "failure",
     failure: error,
   };
 };
 
-export function andThen<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string },
-  U
->(result: Result<T, E>, op: (v: T) => Result<U, E>): Result<U, E> {
+/** @type {<U,T,E extends { [key: string]: string } = { [key in keyof T]: string },>(result: import("./types").Result<T, E>, op: (v: T) => import("./types").Result<U, E>) => import("./types").Result<U, E>} */
+export function andThen(result, op) {
   if (!isOk(result)) {
     return result;
   }
   return op(result.success);
 }
 
-export function safeUnwrap<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(result: SuccessResult<T, E>): T {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").SuccessResult<T, E>) => T} */
+export function safeUnwrap(result) {
   return result.success;
 }
 
-export function unwrap<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(result: OptionalResult<T, E>): T {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").OptionalResult<T, E>) => T} */
+export function unwrap(result) {
   if (isOk(result)) {
     return result.success;
   }
   throw new Error("can't unwrap Err");
 }
 
-export function safeUnwrapErr<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(result: FailureResult<T, E>): E {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").FailureResult<T, E>) => E} */
+export function safeUnwrapErr(result) {
   return result.failure;
 }
 
-export function orDefault<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(result: OptionalResult<T, E>, defaultResult: T): T {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").OptionalResult<T, E>, defaultResult: T) => T} */
+export function orDefault(result, defaultResult) {
   if (isOk(result)) {
     return safeUnwrap(result);
   }
   return defaultResult;
 }
 
-export function mapOr<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string },
-  Q
->(result: OptionalResult<T, E>, fun: (val: T) => Q, defaultResult: Q): Q {
+/** @type {<Q,T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").OptionalResult<T, E>, fun: (val: T) => Q, defaultResult: Q) => Q} */
+export function mapOr(result, fun, defaultResult) {
   if (isOk(result)) {
     return fun(safeUnwrap(result));
   }
   return defaultResult;
 }
 
-export function errOrDefault<
-  T,
-  E extends { [key: string]: string } = { [key in keyof T]: string }
->(result: OptionalResult<T, E>, defaultError: E): E {
+/** @type {<T,E extends { [key: string]: string } = { [key in keyof T]: string }>(result: import("./types").OptionalResult<T, E>, defaultError: E) => E} */
+export function errOrDefault(result, defaultError) {
   if (isErr(result)) {
     return safeUnwrapErr(result);
   }
   return defaultError;
 }
 
-const zoderr2err = (arr: z.ZodIssue[]) => {
+const zoderr2err = (/** @type {z.ZodIssue[]} */ arr) => {
   const initialValue = {};
-  return arr.reduce((acc: Record<string, string>, cval) => {
+  return arr.reduce((/** @type {Record<string, string>} */ acc, cval) => {
     const myAttribute = cval.path[0];
     acc[myAttribute] = (acc[myAttribute] || "") + cval.message + ". ";
     return acc;
   }, initialValue);
 };
 
-export function zod2result<Input, Output>(
-  input:
+/** @type {<Input, Output>(input:
     | {
         success: true;
         data: Output;
@@ -126,7 +97,8 @@ export function zod2result<Input, Output>(
         success: false;
         error: ZodError<Input>;
       }
-): Result<Output> {
+) => import("./types").Result<Output>} */
+export function zod2result(input) {
   if (input.success) {
     return ok(input.data);
   } else {
