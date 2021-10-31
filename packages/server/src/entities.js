721c8821412354137e5fd95b1787e2ca8eed79e1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-11-01 00:39:50 +0100

    Nicer

--- packages/server/src/entities.js
+++ packages/server/src/entities.js
@@ -16,17 +16,29 @@ export function fetchData(table, fieldsToSelect, orderByInfo, query, sanitizedDa
         query.sorting = query.sorting.map(v => [v[0], v[1]==="ASC"?"DESC":"ASC"])
     }
 
-    let queries = query.sorting.map((value, index, array) => {
-        const part = query.sorting.slice(0, index + 1)
+    if (query.sorting.length === 0) {
+        query.sorting = [["id", "ASC"]]
+    }
 
-        let parts = part.map((value, index) => {
-            return sql`${index === part.length - 1 ? (value[1] === "ASC" ? sql`<` : sql`>`) : sql`IS NOT DISTINCT FROM`} ${value[0] ?? null} AND `
-        });
+    const orderByQuery = query.sorting.flatMap(v => [sql`,`, sql`${v[0]} ${v[1]}`]).slice(1)
 
-        return sql`(SELECT ${literal(fieldsToSelect.join(", "))} FROM ${literal(table)} WHERE ${query.paginationCursor === null} OR (${customFilterQuery(sanitizedData)}) AND (${parts}TRUE) LIMIT ${query.paginationLimit+1})`
-    });
+    
+    if (query.paginationCursor === null) {
+        return sql`(SELECT ${literal(fieldsToSelect.join(", "))} FROM ${literal(table)} WHERE ORDER BY ${orderByQuery} LIMIT ${query.paginationLimit+1})`
+    } else {
+        let queries = query.sorting.map((value, index, array) => {
+            const part = query.sorting.slice(0, index + 1)
+    
+            let parts = part.flatMap((value, index) => {
+                return [sql` AND `, sql`${query.paginationCursor[value[0]]??null} ${index === part.length - 1 ? (value[1] === "ASC" ? sql`<` : sql`>`) : sql`IS NOT DISTINCT FROM`} ${value[0] ?? null}`]
+            }).slice(1);
+    
+    
+            return sql`(SELECT ${literal(fieldsToSelect.join(", "))} FROM ${literal(table)} WHERE (${customFilterQuery(sanitizedData)}) AND (${parts}) ORDER BY ${orderByQuery} LIMIT ${query.paginationLimit+1})`
+        });    
 
-    return sql`${queries.flatMap(v => [sql`\nUNION ALL\n`, v]).slice(1)} LIMIT ${query.paginationLimit+1}.`
+        return sql`${queries.flatMap(v => [sql`\nUNION ALL\n`, v]).slice(1)} LIMIT ${query.paginationLimit+1}`
+    }
 }
 
 const value = fetchData("users", ["id", "type", "username", "password_hash"], { "id": "nulls-first", "type": "nulls-first", "username": "nulls-first", "password_hash": "nulls-first" }, {
