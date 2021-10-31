a2a5dd5272c46620a6afc6d45d162d770a90ad40
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-31 02:47:54 +0200

    Design which would be nice

---
+++ packages/projektwahl-lit/src/lib/sql/index.js
@@ -0,0 +1,32 @@
+// https://github.com/porsager/postgres/
+
+// https://www.postgresql.org/docs/current/protocol.html
+
+// postgres can "Extended Query" execute BEGIN; and COMMIT;? seems like yes
+// https://www.postgresql.org/docs/current/protocol-message-types.html
+// https://www.postgresql.org/docs/current/protocol-message-formats.html
+
+// https://github.com/porsager/postgres-benchmarks
+
+/** @typedef {number} Sql */
+
+/**
+ * 
+ * @param {*} strings 
+ * @param  {...any} keys 
+ * @returns {Sql}
+ */
+function sql(strings, ...keys) {
+    return 1
+}
+
+/** @type {any[]} */
+let list = [];
+
+sql`SELECT "id", "title", "info", "place" FROM projects WHERE 1 ${list.map((v) => sql`AND ( ${sql(v)} < 5 )`)} OR NOT ... params() ORDER BY ${list.map(v => sql`${sql(v)} ASC`)} LIMIT 1337`
+
+// an array of Sql gets concatenated
+// an Sql gets inserted
+// other values get prepared-statements
+
+await sql`SELECT 1`.execute()
