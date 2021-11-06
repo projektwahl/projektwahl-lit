c969799ad54a6cf4148f8f02da3e22edeeebe86e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-11-06 19:15:24 +0100

    Start implementing with built-in features

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,10 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
-import express from "express";
-import morgan from "morgan";
-import { v4 as uuidv4 } from "uuid";
-import multer from "multer";
-import bodyParser from "body-parser";
 import { z } from "zod";
 import { get, post } from "./express.js";
 import { zod2result } from "projektwahl-lit-lib/src/result.js";
@@ -19,40 +14,7 @@ import { sql } from './database.js'
 // https://www.npmjs.com/package/zod#comparison
 // https://www.npmjs.com/package/yup
 
-const aMulter = multer({
-  limits: {
-    fieldSize: 10_000,
-    files: 0,
-    parts: 50,
-  },
-});
-
-// https://expressjs.com/en/resources/middleware/compression.html no brotli :(
-// https://expressjs.com/en/resources/middleware/csurf.html
-
-const app = express();
-
-app.use((req, res, next) => {
-  req.id = uuidv4();
-  next();
-});
-
-morgan.token("id", function getId(/** @type {express.Request} */ req) {
-  return req.id;
-});
-
-app.use(
-  morgan(":id :method :url :status :response-time ms - :res[content-length]")
-);
-
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-    parameterLimit: 50,
-  })
-);
-app.use(aMulter.none());
-
+/*
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
   res.send("hello world");
@@ -70,7 +32,7 @@ post(app, "/api/v1/login", async function (req, res) {
     return res.json(loginRequest);
   }
 
-  /** @type {[import("projektwahl-lit-lib/src/types").Existing<Pick<import("projektwahl-lit-lib/src/types").RawUserType, "id"|"username"|"password_hash">>?]} */
+  /** @type {[import("projektwahl-lit-lib/src/types").Existing<Pick<import("projektwahl-lit-lib/src/types").RawUserType, "id"|"username"|"password_hash">>?]} /
   const [dbUser] = await sql`SELECT id, username, password_hash, type FROM users WHERE username = ${loginRequest.success.username} LIMIT 1`;
 
   if (dbUser === undefined) {
@@ -91,7 +53,7 @@ post(app, "/api/v1/login", async function (req, res) {
 		});
 	}
 
-  /** @type {[Pick<import("projektwahl-lit-lib/src/types").RawSessionType, "session_id">]} */
+  /** @type {[Pick<import("projektwahl-lit-lib/src/types").RawSessionType, "session_id">]} /
   const [session] = await sql.begin('READ WRITE', async (sql) => {
 		return await sql`INSERT INTO sessions (user_id) VALUES (${dbUser.id}) RETURNING session_id`;
 	});
@@ -120,11 +82,10 @@ get(app, "/api/v1/sleep", function (req, res) {
   }, 1000);
 });
 
-const server = app.listen(9001);
-
 process.on("SIGINT", () => {
   console.log("SIGINT signal received: closing HTTP server");
   server.close(() => {
     console.log("HTTP server closed");
   });
 });
+*/
--- packages/server/src/password.js
+++ packages/server/src/password.js
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
-import argon2 from 'argon2';
+/*import argon2 from 'argon2';
 
 const argon2Options = {
 	type: argon2.argon2id,
@@ -9,24 +9,63 @@ const argon2Options = {
 	saltLength: 128 / 8, // 16
 	hashLength: 256 / 8, // 32
 	parallelism: 4
-};
+};*/
+import crypto, { webcrypto } from 'node:crypto';
 
 /**
  * 
  * @param {string} password 
- * @returns Promise<string>
+ * @returns {Promise<CryptoKey>}
  */
-export async function hashPassword(password) {
-	// https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-argon2-13
-	return await argon2.hash(password, argon2Options);
+async function getKeyMaterial(password) {
+	let enc = new TextEncoder();
+	return webcrypto.subtle.importKey(
+	  "raw",
+	  enc.encode(password),
+	  "PBKDF2",
+	  false,
+	  ["deriveBits"]
+	);
 }
 
 /**
  * 
- * @param {string} hash 
+ * @param {string} password 
+ * @param {BufferSource} salt 
+ * @returns {Promise<ArrayBuffer>}
+ */
+async function hashPasswordWithSalt(password, salt) {
+	let keyMaterial = await getKeyMaterial(password);
+	return await webcrypto.subtle.deriveBits(
+	  {
+		"name": "PBKDF2",
+		salt: salt,
+		"iterations": 100000,
+		"hash": "SHA-256"
+	  },
+	  keyMaterial,
+	  256
+	);
+}
+
+
+/**
+ * 
+ * @param {string} password 
+ * @returns Promise<[ArrayBuffer, ArrayBuffer]>
+ */
+export async function hashPassword(password) {
+	const salt = webcrypto.getRandomValues(new Uint8Array(64));
+	return [await hashPasswordWithSalt(password, salt), salt];
+}
+
+/**
+ * 
+ * @param {ArrayBuffer} hash 
+ * @param {ArrayBuffer} salt
  * @param {string} password 
  * @returns {Promise<boolean>}
  */
-export async function checkPassword(hash, password) {
-	return await argon2.verify(hash, password, argon2Options);
+export async function checkPassword(hash, salt, password) {
+	return crypto.timingSafeEqual(new Uint8Array(await hashPasswordWithSalt(password, salt)), new Uint8Array(salt));
 }
