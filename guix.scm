965ec33ba28b1d07b1308040bb182b7014447a0b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-12-14 12:13:28 +0100

    Initial packaging attempt

--- guix.scm
+++ guix.scm
@@ -14,10 +14,10 @@
 
 ;; ~/Documents/guix/etc/indent-code.el guix.scm 
 
-;; https://issues.guix.gnu.org/51838#66
-;; [PATCH v3 06/43]
+;; https://issues.guix.gnu.org/51838
 ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=51838
 
+;; guix pull --url=https://gitlab.com/philip1/guix-patches.git --commit=b8a23bcfb5c7bec045b0214f45a85f36ea282ad4 --profile=/tmp/guix.master --disable-authentication
 ;; guix pull --url=/home/moritz/Documents/guix --profile=/tmp/guix.master --disable-authentication
 ;; GUIX_PROFILE="/tmp/guix.master"
 ;; . "$GUIX_PROFILE/etc/profile"
@@ -52,12 +52,22 @@
      '(#:tests?
        #f ; would need additional dependencies
        #:absent-dependencies
-       '("@types/trusted-types")
+       '("@types/trusted-types"
+         "@esm-bundle/chai"
+         "@types/mocha"
+         "@types/web-ie11"
+         "@web/test-runner-mocha"
+         "@webcomponents/shadycss"
+         "@webcomponents/template"
+         "@webcomponents/webcomponentsjs"
+         "chokidar-cli"
+         "concurrently"
+         "internal-scripts"
+         "mocha"
+         "rollup"
+         "typescript")
        #:phases
        (modify-phases %standard-phases
-         ;; The default configure phase fails due to various packages
-         ;; being missing, as we don't have them packaged yet.
-         (delete 'configure)
          (add-after 'unpack 'change-directory
            (lambda _
              (chdir "packages/lit-html")))
@@ -91,18 +101,30 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "3hi9v08jaj8nyiansbk2yaxv21ayya52as6q8viiri5avf8i3nlk"))))
+                "1hi9v08jaj8nyiansbk2yayv21ayya52as6q8viiri5avf8i3nlk"))))
     (build-system node-build-system)
     (arguments
      '(#:tests?
        #f ; would need additional dependencies
        #:absent-dependencies
-       '()
+       '("@babel/cli"
+         "@babel/core"
+         "@babel/plugin-proposal-class-properties"
+         "@babel/plugin-proposal-decorators"
+         "@babel/plugin-transform-typescript"
+         "@esm-bundle/chai"
+         "@types/chai"
+         "@types/mocha"
+         "@webcomponents/shadycss"
+         "@webcomponents/template"
+         "@webcomponents/webcomponentsjs"
+         "chokidar-cli"
+         "internal-scripts"
+         "mocha"
+         "rollup"
+         "typescript")
        #:phases
        (modify-phases %standard-phases
-         ;; The default configure phase fails due to various packages
-         ;; being missing, as we don't have them packaged yet.
-         (delete 'configure)
          (add-after 'unpack 'change-directory
            (lambda _
              (chdir "packages/reactive-element")))
@@ -119,9 +141,6 @@
            ))))
     (native-inputs
      `(("esbuild" ,esbuild)))
-    (inputs
-      `(("lit-html" ,lit-html)
-        ("@lit/reactive-element" ,lit-reactive-element)))
     (home-page "https://github.com/lit/lit")
     (synopsis "simple library for building fast, lightweight web components.")
     (description "Lit is a simple library for building fast, lightweight web components.")
@@ -140,16 +159,27 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "3hi9v08jaj8nyiansbk2yayv21ayya52as6q8viiri5avf8i3nlk"))))
+                "1hi9v08jaj8nyiansbk2yayv21ayya52as6q8viiri5avf8i3nlk"))))
     (build-system node-build-system)
     (arguments
      '(#:tests?
        #f ; would need additional dependencies
+       #:absent-dependencies
+       '("@esm-bundle/chai"
+         "@types/chai"
+         "@types/mocha"
+         "@webcomponents/shadycss"
+         "@webcomponents/template"
+         "@webcomponents/webcomponentsjs"
+         "chokidar-cli"
+         "downlevel-dts"
+         "internal-scripts"
+         "mocha"
+         "rollup"
+         "tslib"
+         "typescript")
        #:phases
        (modify-phases %standard-phases
-         ;; The default configure phase fails due to various packages
-         ;; being missing, as we don't have them packaged yet.
-         ;(delete 'configure)
          (add-after 'unpack 'change-directory
            (lambda _
              (chdir "packages/lit-element")))
@@ -166,6 +196,9 @@
            ))))
     (native-inputs
      `(("esbuild" ,esbuild)))
+    (inputs
+     `(("lit-html" ,lit-html)
+       ("@lit/reactive-element" ,lit-reactive-element)))
     (home-page "https://github.com/lit/lit")
     (synopsis "simple library for building fast, lightweight web components.")
     (description "Lit is a simple library for building fast, lightweight web components.")
