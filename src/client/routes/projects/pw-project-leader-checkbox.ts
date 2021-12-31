ddcd039f574835939d62e4aa0b42f9f7f3a8bebb
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-12-31 23:21:22 +0100

    This is the design

--- src/client/routes/projects/pw-project-leader-checkbox.ts
+++ src/client/routes/projects/pw-project-leader-checkbox.ts
@@ -1,19 +1,21 @@
 import "../../form/pw-checkbox-input.js";
-import { Task } from "@lit-labs/task";
+import { Task, TaskStatus } from "@lit-labs/task";
 import { html, LitElement } from "lit";
 import { HistoryController } from "../../history-controller.js";
 import { myFetch } from "../../utils.js";
 import { createRef, ref } from "lit/directives/ref.js";
+import { msg } from "@lit/localize";
+import { bootstrapCss } from "../../index.js";
 
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 
 class PwProjectLeaderCheckbox extends LitElement {
-
     static override get properties() {
         return {
           _task: { state: true },
           forceTask: { state: true },
+          disabled: { state: true },
         };
       }
 
@@ -24,10 +26,14 @@ class PwProjectLeaderCheckbox extends LitElement {
     any
   >;
 
+  disabled: boolean;
+
   form: import("lit/directives/ref").Ref<HTMLFormElement>;
 
   constructor() {
     super();
+
+    this.disabled = false;
 
     this.form = createRef();
 
@@ -36,38 +42,58 @@ class PwProjectLeaderCheckbox extends LitElement {
     this._task = new Task(
       this,
       async () => {
-        const formDataEvent = new CustomEvent("myformdata", {
-          bubbles: true,
-          composed: true,
-          detail: {},
-        });
-        this.form.value?.dispatchEvent(formDataEvent);
-
-        let result = await myFetch("/api/v1/login", {
+        let result = await myFetch("/api/v1/users/create-or-update", {
           method: "POST",
           headers: {
             "content-type": "text/json",
           },
-          body: JSON.stringify(formDataEvent.detail),
+          body: JSON.stringify({
+            id: 1337,
+            project_leader_id: 1338
+          }),
         });
-
-        if (result.success) {
-          HistoryController.goto(new URL("/", window.location.href), {});
-        }
 
         return result;
       },
-      () => [this.forceTask]
+      () => [this.forceTask] // TODO FIXME ensure this is working and update to the new method
     );
   }
 
   render() {
-    return html`<form ${ref(this.form)}>
-      <pw-checkbox-input
-        name="is_project_leader"
-        label="test"
-        .task=${"hi"}
-      ></pw-checkbox-input>
+    return html`
+          ${bootstrapCss}
+          <form ${ref(this.form)}>
+             ${this._task.render({
+              complete: (data) => {
+                if (!data.success) {
+                  const errors = Object.entries(data.error)
+                    .map(([k, v]) => html`${k}: ${v}<br />`);
+                  if (errors.length > 0) {
+                    return html`<div class="alert alert-danger" role="alert">
+                      ${msg("Some errors occurred!")}<br />
+                      ${errors}
+                    </div>`;
+                  }
+                }
+                return html``;
+              },
+              error: (error) => html`<div
+                class="alert alert-danger"
+                role="alert"
+              >
+                ${error}
+              </div>`,
+            })}
+
+        <input
+          @change=${() => {
+            this.forceTask = (this.forceTask || 0) + 1;
+          }}
+          type="checkbox"
+          ?disabled=${this._task.status === TaskStatus.PENDING}
+          checked=${true/* TODO FIXME */}
+          class="form-check-input"
+        />
     </form>`;
   }
 }
