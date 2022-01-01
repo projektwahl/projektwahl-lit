6399c6651eeb53d3dcc1dfe24d2733152fc88eb1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-01 22:49:24 +0100

    argon2id

--- src/server/password.ts
+++ src/server/password.ts
@@ -6,10 +6,15 @@ import argon2 from 'argon2';
 // for now probably not too important as almost everybody should be using OpenID
 // we could also additionally do the PBKDF2 client side so no log leaks etc would be possible.
 
+// https://datatracker.ietf.org/doc/html/rfc9106#section-4
+// If much less memory is available, a uniformly safe option is
+// Argon2id with t=3 iterations, p=4 lanes, m=2^(16) (64 MiB of
+// RAM), 128-bit salt, and 256-bit tag size.  This is the SECOND
+// RECOMMENDED option.
 const argon2Options = {
 	type: argon2.argon2id,
 	timeCost: 3,
-	memoryCost: 64 * 1024, // 64 MiB
+	memoryCost: 2 ** 16, // 2 ** 16 KB (64 MB)
 	saltLength: 128 / 8, // 16
 	hashLength: 256 / 8, // 32
 	parallelism: 4
