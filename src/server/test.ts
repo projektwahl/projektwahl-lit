0b68eba3cf06371274cef8d592a894063c2fcc22
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-02 19:37:51 +0100

    This may work

--- src/server/test.ts
+++ src/server/test.ts
@@ -5,15 +5,13 @@ import { entityRoutes } from "../lib/routes.js";
 // https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
 
 type entitesType = {
-    [K in keyof typeof entityRoutes]: typeof entityRoutes[K]//["response"]//["options"][0]["shape"]["data"]["shape"]["entities"];
+    [K in keyof typeof entityRoutes]: typeof entityRoutes[K]
 };
 
 export async function fetchData<R extends keyof typeof entityRoutes>(path: R): z.infer<typeof entityRoutes[R]["response"]> {
-        
-    let entitySchema: entitesType[R] = entityRoutes[path] //["response"]//["options"][0]["shape"]["data"]["shape"]["entities"];
-    
-    
+    let entitySchema: entitesType[R] = entityRoutes[path];
 
-    entitySchema.parse({})
+    let b: entitesType[R]["response"]["options"][0]["shape"]["data"]["shape"]["entities"] = entitySchema["response"]["options"][0]["shape"]["data"]["shape"]["entities"];
 
+    //entitySchema.parse({})
 }
