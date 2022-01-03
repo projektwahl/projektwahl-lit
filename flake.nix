f954bb517dd6cb5d5c2f0c1457a21a84ccf3650b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-03 20:30:08 +0100

    Add brotli

--- flake.nix
+++ flake.nix
@@ -45,6 +45,7 @@ SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
               #pkgs.lp_solve
               #pkgs.diffoscope
               pkgs.python3 # argon2
+              pkgs.brotli
             ];
           };
 
