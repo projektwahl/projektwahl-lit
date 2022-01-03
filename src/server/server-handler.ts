70342ab4aec0d9dba735d1e381c42fa9162cee19
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-03 23:05:15 +0100

    Use svgs inline

--- src/server/server-handler.ts
+++ src/server/server-handler.ts
@@ -241,17 +241,13 @@ export async function serverHandler(
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
         crossorigin="anonymous"
       />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css"
-      />
   
       <title>Hello, world!</title>
     </head>
     <body>
       <script
         type="module"
-        src="/dist/pw-app.js"
+        src="/src/client/pw-app.js"
       ></script>
       <noscript>Bitte aktiviere JavaScript!</noscript>
   
