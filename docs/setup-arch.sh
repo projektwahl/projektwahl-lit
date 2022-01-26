dad117a66dcd710e76c24e9a8bb705d5e4a53d94
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-26 18:12:26 +0100

    Update setup-arch.sh

--- docs/setup-arch.sh
+++ docs/setup-arch.sh
@@ -1,1 +1,17 @@
+# hetzner rescue mode
+ssh root@162.55.211.18
+# https://community.hetzner.com/tutorials/install-ubuntu-2004-with-full-disk-encryption
+# https://madflex.de/install-archlinux-on-hetzner-cloud/
+# https://github.com/kevinveenbirkenbach/hetzner-arch-luks
+
+CRYPTPASSWORD secret
+DRIVE1 /dev/sda
+BOOTLOADER grub
+HOSTNAME host.example.com
+PART /boot ext4 1G
+PART /     ext4 all crypt
+IMAGE /root/images/Ubuntu-2004-focal-64-minimal.tar.gz
+SSHKEYS_URL /tmp/authorized_keys
+
+installimage
 
