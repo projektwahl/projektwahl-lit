d10c1981f759b5afa41d4791f53dec63b86b8e39
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-26 18:36:40 +0100

    Update setup-arch.sh

--- docs/setup-arch.sh
+++ docs/setup-arch.sh
@@ -3,15 +3,41 @@ ssh root@162.55.211.18
 # https://community.hetzner.com/tutorials/install-ubuntu-2004-with-full-disk-encryption
 # https://madflex.de/install-archlinux-on-hetzner-cloud/
 # https://github.com/kevinveenbirkenbach/hetzner-arch-luks
+# https://wiki.archlinux.org/title/Dm-crypt/Specialties#Remote_unlocking_(hooks:_systemd,_systemd-tool)
+# https://github.com/random-archer/mkinitcpio-systemd-tool
 
-CRYPTPASSWORD secret
+nano /tmp/setup.conf
 DRIVE1 /dev/sda
 BOOTLOADER grub
-HOSTNAME host.example.com
+HOSTNAME aes.selfmade4u.de
 PART /boot ext4 1G
 PART /     ext4 all crypt
-IMAGE /root/images/Ubuntu-2004-focal-64-minimal.tar.gz
+IMAGE /root/.oldroot/nfs/install/../images/archlinux-latest-64-minimal.tar.gz
 SSHKEYS_URL /tmp/authorized_keys
+CRYPTPASSWORD secret
 
-installimage
+nano /tmp/authorized_keys
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpm6jXKndgHfeANK/Dipr2f5x75EDY17/NfUieutEJ4 moritz@nixos
+
+nano /tmp/post-install.sh
+#!/bin/bash
+
+pacman -S mkinitcpio-systemd-tool
+cat /etc/mkinitcpio.conf
+echo "HOOKS=(base autodetect modconf block filesystems keyboard fsck systemd systemd-tool)" > /etc/mkinitcpio.conf
+
+edit /etc/mkinitcpio-systemd-tool/config/crypttab
+edit /etc/mkinitcpio-systemd-tool/config/fstab
+systemctl enable initrd-cryptsetup.path
+systemctl enable initrd-tinysshd.service
+systemctl enable initrd-debug-progs.service
+systemctl enable initrd-sysroot-mount.service
+
+mkinitcpio -v -p linux > build.log
+lsinitcpio -l /boot/initramfs-linux.img
+
+chmod +x /tmp/post-install.sh
+
+
+installimage -a -c /tmp/setup.conf -x /tmp/post-install.sh
 
