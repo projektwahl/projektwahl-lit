c79c3f54a4d586365fb04fd1df1ded3748be65ca
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-27 15:19:33 +0100

    Update setup-arch-luks2-grub.sh

--- docs/setup-arch-luks2-grub.sh
+++ docs/setup-arch-luks2-grub.sh
@@ -1,1 +1,8 @@
-# boot into recovery
+# boot into rescue
+# https://wiki.archlinux.org/title/Installation_guide
+# https://wiki.archlinux.org/title/Install_Arch_Linux_from_existing_Linux
+git clone https://github.com/archlinux/arch-install-scripts
+cd arch-install-scripts/
+apt update
+apt install m4
+make
