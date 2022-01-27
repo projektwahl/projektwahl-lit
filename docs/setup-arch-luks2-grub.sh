3537f8491cffee8ccabc56aeb8e8346c7a54c5aa
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-27 16:25:13 +0100

    Update setup-arch-luks2-grub.sh

--- docs/setup-arch-luks2-grub.sh
+++ docs/setup-arch-luks2-grub.sh
@@ -3,11 +3,13 @@
 # https://wiki.archlinux.org/title/Install_Arch_Linux_from_existing_Linux
 ssh root@168.119.156.152
 timedatectl set-ntp true
-fdisk /dev/sda
-g
-n
-# ...
-w
-cryptsetup open --type plain -d /dev/urandom /dev/sda1
+
+cryptsetup open --type plain -d /dev/urandom /dev/sda
 dd if=/dev/zero of=/dev/mapper/to_be_wiped status=progress bs=1M 
 cryptsetup close to_be_wiped
+
+parted /dev/sda mklabel gpt mkpart boot boot 0MiB 1MiB set boot bios_grub on mkpart root ext4 1MiB 100% 
+
+# https://wiki.archlinux.org/title/Dm-crypt/Encrypting_an_entire_system#Encrypted_boot_partition_(GRUB)
+# https://wiki.archlinux.org/title/GRUB#Encrypted_/boot
+cryptsetup luksFormat --type luks2 --pbkdf pbkdf2 /dev/sda1
