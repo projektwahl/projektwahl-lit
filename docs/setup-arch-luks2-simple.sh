0d137f8f4a072c2ffc3d0bb15778fb7871dbb6a9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-27 22:52:24 +0100

    Install arch (#88)

---
+++ docs/setup-arch-luks2-simple.sh
@@ -0,0 +1,136 @@
+# boot with iso mounted
+# https://wiki.archlinux.org/title/Installation_guide
+# https://wiki.archlinux.org/title/Install_Arch_Linux_from_existing_Linux
+# https://wiki.archlinux.org/title/Dm-crypt/Encrypting_an_entire_system
+ssh root@168.119.156.152
+
+cryptsetup open --type plain -d /dev/urandom /dev/sda to_be_wiped
+dd if=/dev/zero of=/dev/mapper/to_be_wiped status=progress bs=1M 
+cryptsetup close to_be_wiped
+
+sgdisk --clear /dev/sda
+sgdisk -n 0:0:+1MiB -t 0:ef02 -c 0:grub /dev/sda
+sgdisk -n 0:0:+512MiB -t 0:ea00 -c 0:boot /dev/sda
+sgdisk -n 0:0:0 -t 0:8300 -c 0:root /dev/sda
+sgdisk --print /dev/sda 
+
+cryptsetup luksFormat --type luks2 /dev/sda3
+cryptsetup open /dev/sda3 root
+mkfs.ext4 /dev/mapper/root
+mount /dev/mapper/root /mnt
+
+mkfs.ext4 /dev/sda2
+mkdir /mnt/boot
+mount /dev/sda2 /mnt/boot
+
+pacstrap /mnt base linux
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
+echo "arch-luks2" >> /etc/hostname
+
+echo "HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt filesystems fsck)" >> /etc/mkinitcpio.conf
+
+mkinitcpio -P
+
+passwd
+
+pacman -S --noconfirm nano grub
+
+blkid | grep /dev/sda3
+echo 'GRUB_CMDLINE_LINUX="cryptdevice=UUID=a1109faa-dca2-4ccd-857b-7ce4e663a91e:root root=/dev/mapper/root"' >> /etc/default/grub
+
+grub-install --target=i386-pc --recheck /dev/sda
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# https://wiki.archlinux.org/title/Systemd-networkd
+
+systemctl enable systemd-networkd
+nano /etc/systemd/network/20-wired.network
+[Match] 
+Name=en*
+
+[Network]
+Address=2a01:4f8:c010:4c33::1/64
+Gateway=172.31.1.1
+Gateway=fe80::1
+DNS=185.12.64.1
+DNS=185.12.64.2
+
+[Address]
+Address=168.119.156.152/32
+Peer=172.31.1.1/32
+
+systemctl enable systemd-resolved
+
+
+pacman -S openssh
+systemctl enable sshd
+
+
+exit
+sudo umount /mnt/boot
+sudo umount /mnt
+cryptsetup close root
+reboot
+
+# recover from liveiso
+mount /dev/sda3 /mnt
+mount /dev/sda2 /mnt/boot
+arch-chroot /mnt
+cryptsetup close root
+
+
+
+
+
+
+
+
+
+pacman -S sudo
+visudo
+# uncomment wheel line
+
+useradd -m -G wheel moritz
+passwd moritz
+exit
+ssh-copy-id moritz@168.119.156.152
+
+ssh moritz@168.119.156.152
+sudo nano /etc/ssh/sshd_config
+Port 2121
+PermitRootLogin no      
+PasswordAuthentication no
+AuthenticationMethods publickey
+
+sudo systemctl restart sshd
+ssh moritz@168.119.156.152 -p 2121
+
+sudo pacman -S ufw
+sudo ufw default deny
+sudo ufw allow 2121
+sudo ufw logging off
+sudo ufw enable
+
+sudo pacman -S postgresql
+sudo -iu postgres initdb -D /var/lib/postgres/data
+sudo systemctl --now enable postgresql
+sudo pacman -S nodejs
+
+sudo pacman -S nginx-mainline
+sudo systemctl enable --now nginx
+sudo ufw allow http
+sudo ufw allow https
+
+
+sudo nano /etc/nginx/nginx.conf
+# edit server_name aes.selfmade4u.de
+sudo pacman -S certbot-nginx
+sudo certbot --nginx -d aes.selfmade4u.de -m Moritz.Hedtke@t-online.de --agree-tos -n
