0a623bfabf1a52d2d5085d5af3c61eeee88ba163
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-27 15:46:34 +0100

    Update setup-arch-luks2-grub.sh

--- docs/setup-arch-luks2-grub.sh
+++ docs/setup-arch-luks2-grub.sh
@@ -1,8 +1,13 @@
-# boot into rescue
+# boot with iso mounted
 # https://wiki.archlinux.org/title/Installation_guide
 # https://wiki.archlinux.org/title/Install_Arch_Linux_from_existing_Linux
-git clone https://github.com/archlinux/arch-install-scripts
-cd arch-install-scripts/
-apt update
-apt install m4
-make
+ssh root@168.119.156.152
+timedatectl set-ntp true
+fdisk /dev/sda
+g
+n
+# ...
+w
+cryptsetup open --type plain -d /dev/urandom /dev/sda1
+dd if=/dev/zero of=/dev/mapper/to_be_wiped status=progress bs=1M 
+cryptsetup close to_be_wiped
