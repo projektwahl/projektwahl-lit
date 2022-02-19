bce84d21234d64ad73bc5991bdc49b38f8c1c8a6
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-02-19 21:30:01 +0100

    Jo

--- build.js
+++ build.js
@@ -2,6 +2,44 @@ import { exec as unpromisifiedExec } from "child_process";
 import { createHash } from "crypto";
 import { readFile, rename, writeFile } from "fs/promises";
 import { promisify } from "util";
+import './require-shim.js';
+import { build } from "esbuild";
+
+const nativeNodeModulesPlugin = {
+  name: 'native-node-modules',
+  setup(build) {
+    // If a ".node" file is imported within a module in the "file" namespace, resolve 
+    // it to an absolute path and put it into the "node-file" virtual namespace.
+    build.onResolve({ filter: /\.node$/, namespace: 'file' }, args => ({
+      path: require.resolve(args.path, { paths: [args.resolveDir] }),
+      namespace: 'node-file',
+    }))
+
+    // Files in the "node-file" virtual namespace call "require()" on the
+    // path from esbuild of the ".node" file in the output directory.
+    build.onLoad({ filter: /.*/, namespace: 'node-file' }, args => ({
+      contents: `
+        import path from ${JSON.stringify(args.path)}
+        try { module.exports = require(path) }
+        catch {}
+      `,
+    }))
+
+    // If a ".node" file is imported within a module in the "node-file" namespace, put
+    // it in the "file" namespace where esbuild's default loading behavior will handle
+    // it. It is already an absolute path since we resolved it to one above.
+    build.onResolve({ filter: /\.node$/, namespace: 'node-file' }, args => ({
+      path: args.path,
+      namespace: 'file',
+    }))
+
+    // Tell esbuild's default loading behavior to use the "file" loader for
+    // these ".node" files.
+    let opts = build.initialOptions
+    opts.loader = opts.loader || {}
+    opts.loader['.node'] = 'file'
+  },
+}
 
 const exec = promisify(unpromisifiedExec);
 /*
@@ -110,10 +148,32 @@ await writeFile("dist/index.html", index);
 }
 
 {
+  /*
   let { stdout, stderr } = await exec(
-    `esbuild --platform=node --format=esm --bundle src/server/index.ts --external:@dev.mohe/argon2 --define:process.env.NODE_ENV=\\"production\\" --charset=utf8 --entry-names=[dir]/[name] --sourcemap --analyze --outfile=dist/server.js --inject:./require-shim.js --minify-whitespace --minify-identifiers --minify-syntax --tree-shaking=true --minify`
+    `esbuild --platform=node --format=esm --bundle src/server/index.ts --external:@dev.mohe/argon2/build/Release/argon2.node --define:process.env.NODE_ENV=\\"production\\" --charset=utf8 --entry-names=[dir]/[name] --sourcemap --analyze --outfile=dist/server.js --inject:./require-shim.js --minify-whitespace --minify-identifiers --minify-syntax --tree-shaking=true --minify --loader:.node=./esbuild-plugin-node-extension.js`
   );
 
   console.log(stdout);
   console.log(stderr);
+  */
+
+  await build({
+    platform: "node",
+    format: "esm",
+    bundle: true,
+    entryPoints: ["src/server/index.ts"],
+    define: {
+      "process.env.NODE_ENV": "\"production\"",
+    },
+    charset: "utf8",
+    sourcemap: true,
+    outfile: "dist/server.js",
+    inject: ["./require-shim.js"],
+    minify: true,
+    minifyIdentifiers: true,
+    minifyWhitespace: true,
+    minifySyntax: true,
+    treeShaking: true,
+    plugins: [nativeNodeModulesPlugin],
+  })
 }
