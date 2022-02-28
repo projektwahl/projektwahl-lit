2af619f186d63e32f22d5ead866dd9b41134aa8c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-02-28 22:05:22 +0100

    Jo

--- src/server/routes/evaluate/index.ts
+++ src/server/routes/evaluate/index.ts
@@ -5,6 +5,7 @@ import { constants } from "node:fs";
 import { execFile } from "node:child_process";
 import { sql } from "../../database.js";
 import groupBy from "lodash-es/groupBy.js";
+import sortedIndexBy from "lodash-es/sortedIndexBy.js";
 
 export class CPLEXLP {
   constructor() {}
@@ -70,19 +71,17 @@ export class CPLEXLP {
     await this.fileHandle.write(` ${name}`);
   };
 
-  endVariables = async () => {
-  };
+  endVariables = async () => {};
 
   startBinaryVariables = async () => {
     await this.fileHandle.write(`\nBinary\n`);
-  }
+  };
 
   binaryVariable = async (name: string) => {
     await this.fileHandle.write(` ${name}`);
-  }
+  };
 
-  endBinaryVariables = async () => {
-  }
+  endBinaryVariables = async () => {};
 
   calculate = async () => {
     await this.fileHandle.write(`\nEnd\n`);
@@ -138,6 +137,13 @@ const choices = await sql.file("src/server/routes/evaluate/calculate.sql", [], {
   cache: false,
 });
 
+// TODO FIXME database transaction to ensure consistent view of data
+const projects =
+  await sql`SELECT id, min_participants, max_participants FROM projects;`;
+
+const users =
+  await sql`SELECT id, project_leader_id FROM present_voters ORDER BY id;`;
+
 console.log(choices);
 
 await lp.startMaximize();
@@ -153,10 +159,6 @@ await lp.endMaximize();
 
 await lp.startConstraints();
 
-// TODO FIXME database transaction to ensure consistent view of data
-const projects = await sql
-`SELECT id, min_participants, max_participants FROM projects;`;
-
 // TODO FIXME project leader should add a fake 0 choice?
 // only in one project or project leader
 for (const groupedChoice of Object.entries(
@@ -165,56 +167,64 @@ for (const groupedChoice of Object.entries(
   await lp.constraint(
     `only_in_one_project_${groupedChoice[0]}`,
     0,
-    groupedChoice[1].map((choice) => [
-      1,
-      `choice_${choice.user_id}_${choice.project_id}`,
-    ]),
+    [
+      ...groupedChoice[1].map<[number, string]>((choice) => [
+        1,
+        `choice_${choice.user_id}_${choice.project_id}`,
+      ]),
+      ...(sortedIndexBy(users, { id: groupedChoice[0] }, (u) => u.id)
+        ? ([[1, `project_leader_${groupedChoice[0]}`]] as [number, string][])
+        : []),
+    ],
     1
   );
 }
 
 // only in project if it exists
 for (const choice of choices) {
-    await lp.constraint(
-        `project_must_exist_${choice.user_id}_${choice.project_id}`,
-        0,
-        [[1, `choice_${choice.user_id}_${choice.project_id}`], [1, `project_not_exists_${choice.project_id}`]],
-        1
-      );
-  }
-
-const users =
-      await sql
-      `SELECT id, project_leader_id FROM present_voters;`;
+  await lp.constraint(
+    `project_must_exist_${choice.user_id}_${choice.project_id}`,
+    0,
+    [
+      [1, `choice_${choice.user_id}_${choice.project_id}`],
+      [1, `project_not_exists_${choice.project_id}`],
+    ],
+    1
+  );
+}
 
 // either project leader or project does not exist
 for (const user of users) {
-    if (user.project_leader_id === null) continue;
-    await lp.constraint(
-        `either_project_leader_or_project_not_exists_${user.id}`,
-        0,
-        [[1, `project_leader_${user.id}`], [1, `project_not_exists_${user.project_leader_id}`]],
-        1
-      );
+  if (user.project_leader_id === null) continue;
+  await lp.constraint(
+    `either_project_leader_or_project_not_exists_${user.id}`,
+    0,
+    [
+      [1, `project_leader_${user.id}`],
+      [1, `project_not_exists_${user.project_leader_id}`],
+    ],
+    1
+  );
 }
 
 await lp.startVariables();
 await lp.endVariables();
 
-await lp.startBinaryVariables()
+await lp.startBinaryVariables();
 
 for (const choice of choices) {
   await lp.binaryVariable(`choice_${choice.user_id}_${choice.project_id}`);
 }
 
+// TODO FIXME remove this completely as its implied by the project_not_exists
 for (const user of users) {
-    await lp.binaryVariable(`project_leader_${user.id}`);
+  await lp.binaryVariable(`project_leader_${user.id}`);
 }
 
 for (const project of projects) {
-    await lp.binaryVariable(`project_not_exists_${project.id}`);
+  await lp.binaryVariable(`project_not_exists_${project.id}`);
 }
 
-await lp.endBinaryVariables()
+await lp.endBinaryVariables();
 
 await lp.calculate();
