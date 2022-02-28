fce4b5e6aa0110cc4adf906bd8fc2794b79ac43c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-02-28 20:42:38 +0100

    Jo

--- stacktrace-source-map.js
+++ stacktrace-source-map.js
@@ -1,5 +1,7 @@
-import sourceMap from 'source-map';
-import { readFileSync } from 'fs';
+import sourceMap from "source-map";
+import { readFileSync } from "fs";
 
-var smc = new sourceMap.SourceMapConsumer(readFileSync("dist/pw-app.js.map","utf8"));
-console.log(smc.originalPositionFor({line: 5, column: 2290}));
+var smc = new sourceMap.SourceMapConsumer(
+  readFileSync("dist/pw-app.js.map", "utf8")
+);
+console.log(smc.originalPositionFor({ line: 5, column: 2290 }));
