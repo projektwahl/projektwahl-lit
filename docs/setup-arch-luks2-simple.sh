9617397e2abd56bf3e9f52b2abffef84fb6a963e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-04 21:18:03 +0100

    Update setup-arch-luks2-simple.sh

--- docs/setup-arch-luks2-simple.sh
+++ docs/setup-arch-luks2-simple.sh
@@ -316,7 +316,7 @@ git clone git@github.com:projektwahl/projektwahl-lit.git projektwahl-lit-staging
 cd projektwahl-lit-staging
 openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' -keyout key.pem -out cert.pem
 npm ci --ignore-scripts --omit=optional
-npx node-gyp rebuild -C ./node_modules/@dev.mohe/argon2/
+node-gyp rebuild -C ./node_modules/@dev.mohe/argon2/
 npm run build
 
 sudo mkdir projektwahl-lit-production
@@ -325,7 +325,7 @@ git clone git@github.com:projektwahl/projektwahl-lit.git projektwahl-lit-product
 cd projektwahl-lit-production
 openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' -keyout key.pem -out cert.pem
 npm ci --ignore-scripts --omit=optional
-npx node-gyp rebuild -C ./node_modules/@dev.mohe/argon2/
+node-gyp rebuild -C ./node_modules/@dev.mohe/argon2/
 npm run build
 
 
