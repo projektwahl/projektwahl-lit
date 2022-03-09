a2070cc0b342460fa1753d8f8f0e4247b8a3e657
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-09 20:55:15 +0100

    Fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,7 +35,7 @@ module.exports = {
     node: true,
   },
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
     "@typescript-eslint/consistent-type-assertions": [
       "error",
       { assertionStyle: "never" },
