aaf30233980f477ea7430600abc9ed8b09b17865
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-10 21:44:14 +0100

    Cleanup converter (also added instructions to the wiki)

--- csv2json.js
+++ csv2json.js
@@ -1,22 +1,24 @@
 import { parse } from "csv-parse";
 import { createReadStream } from "node:fs";
+import { exit } from "node:process";
 
-const parser = createReadStream("")
+if (process.argv.length !== 3) {
+  console.log("usage: node csv2json.js <file>")
+  exit(0)
+}
+
+const parser = createReadStream(process.argv[2])
   .pipe(
     parse({
       trim: true,
       columns: true,
-      delimiter: ";",
+      delimiter: ",",
       cast: true,
     })
   );
 
 const result = []
 for await (const entry of parser) {
-	result.push({
-		// TODO FIXME these could / should be done in LibreOffice or so (so this script doesnt need to be adapted. Also maybe we should provide an upload for csv as it doesnt seem too complicated and is a pretty common format)
-		name: `${entry["Vorname"]} ${entry["Nachname"]}`,
-		openid_id: entry["E-Mail"],
-	})
+	result.push(entry)
 }
 console.log(JSON.stringify(result, null, 2))
