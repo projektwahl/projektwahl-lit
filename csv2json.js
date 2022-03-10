a1d67a09c79edfb9bc77163cfb3e6259db5d3838
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-10 14:42:59 +0100

    Note

--- csv2json.js
+++ csv2json.js
@@ -14,6 +14,7 @@ const parser = createReadStream("")
 const result = []
 for await (const entry of parser) {
 	result.push({
+		// TODO FIXME these could / should be done in LibreOffice or so (so this script doesnt need to be adapted. Also maybe we should provide an upload for csv as it doesnt seem too complicated and is a pretty common format)
 		name: `${entry["Vorname"]} ${entry["Nachname"]}`,
 		openid_id: entry["E-Mail"],
 	})
