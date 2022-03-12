2bc3d1f18800889a2e33e9bb513ee5be7eab5160
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-12 23:23:48 +0100

    Jo

--- src/server/express.ts
+++ src/server/express.ts
@@ -128,10 +128,10 @@ export function requestHandler<P extends keyof typeof routes>(
             ...defaultHeaders,
             ...finalHeaders,
           });
-          const stringified = JSON.stringify(responseBody)
+          const stringified = JSON.stringify(responseBody);
           setImmediate(() => {
             response.end(stringified);
-          })
+          });
         } else {
           // https://github.com/colinhacks/zod/blob/master/ERROR_HANDLING.md
 
