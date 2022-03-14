bc87ba9d432dd3e6f863600d1049a52540707eef
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-14 21:38:53 +0100

    reuse lint fixes

--- docs/setup-arch-luks2-simple.sh
+++ docs/setup-arch-luks2-simple.sh
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 # boot with iso mounted
 # https://wiki.archlinux.org/title/Installation_guide
 # https://wiki.archlinux.org/title/Install_Arch_Linux_from_existing_Linux
