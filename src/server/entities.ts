4db5e1e1e0eeae54d4b8bd669978797bd906711f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-24 18:44:04 +0100

    How

--- src/server/entities.ts
+++ src/server/entities.ts
@@ -31,7 +31,7 @@ type ToIndexed<T extends { [K: string]: Array<any> }, I extends number> = {
   [K in keyof T]: T[K][I]
 }
 
-export function mappedIndexing<T extends { [K: string]: Array<any> }, K extends string, I extends number>(value: T[K], index: I): ToIndexed<T, I> {
+export function mappedIndexing<T extends { [K: string]: Array<any> }, K extends string, I extends number>(value: T[K], index: I): ToIndexed<T, I>[K] {
   return value[index];
 }
 
@@ -123,7 +123,7 @@ export async function fetchData<R extends keyof typeof entityRoutes>(
       const v0: entitiesType6[R] = v[0];
       // @ts-expect-error bruh
       const v1: entitiesType2[R][typeof v0] = v[1];
-      const v2: entitiesType10[R] = v[2];
+      const v2: entitiesType10[R] = mappedIndexing(v, 2);
       return [
         sql`,`,
         // @ts-expect-error bruh
