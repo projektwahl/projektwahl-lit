c5fa225701d697ed19f62ca923ec35cefcb75705
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-03-24 23:09:55 +0100

    This check seems to work

--- src/server/routes/users/create-or-update.ts
+++ src/server/routes/users/create-or-update.ts
@@ -93,6 +93,16 @@ export const createOrUpdateUsersHandler = requestHandler(
     // for setting in_project_id to null we need to check the db if the user was in this particular project previously
     //if (loggedInUser?.type === "helper" && users.every(user => user.action === "update" && user.))
 
+    // if in_project_id is not null we need to check that no old value is overwritten
+
+    // maybe do the check in a transaction? or use a trigger?
+
+    /*
+    SELECT COUNT(*) FROM users_with_deleted WHERE (id = 5 and type = 'voter' AND (users_with_deleted.project_leader_id IS NULL AND 105 IS NOT NULL) OR (users_with_deleted.project_leader_id = 105 AND NULL IS NULL)) OR (id = 2106 AND type = 'helper' AND project_leader_id = 105);
+
+
+    */
+
     try {
       const row = (
         await sql.begin("READ WRITE", async (sql) => {
