e850fef22f1798ca4008f58049f6d8d5477eade7
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-03 22:27:46 +0200

    More tesiting progress

--- tests/e2e/test-calculate.ts
+++ tests/e2e/test-calculate.ts
@@ -1,6 +1,7 @@
 import { sql } from "../../src/server/database.js";
 import { Chance } from "chance";
 import { evaluate } from "../../src/server/routes/evaluate/index.js";
+import { userInfo } from "os";
 
 const chance = new Chance();
 
@@ -20,7 +21,19 @@ export async function project(
   max_age: number = 13,
   random_assignments: boolean = true
 ) {
-  sql`INSERT INTO projects (title, info, place, costs, min_age, max_age, min_participants, max_participants, random_assignments, last_updated_by) VALUES (${chance.sentence()}, '', '', 0, ${min_age}, ${max_age}, ${min_participants}, ${max_participants}, ${random_assignments})`;
+  return (await sql`INSERT INTO projects (title, info, place, costs, min_age, max_age, min_participants, max_participants, random_assignments, last_updated_by) VALUES (${chance.sentence()}, '', '', 0, ${min_age}, ${max_age}, ${min_participants}, ${max_participants}, ${random_assignments}) RETURNING projects.id;`)[0].id;
+}
+
+export async function user(age: number) {
+    return (await sql`INSERT INTO users (username, type, "group", age, last_updated_by) VALUES (${chance.name(
+        { prefix: true, suffix: true }
+      )}, 'voter', '', ${age}, 1) RETURNING users.id;`)[0].id
+}
+
+export async function vote(project_id: number, user_id: number, rank: number) {
+    await sql`INSERT INTO choices (user_id, project_id, rank) VALUES (${
+        user_id
+      }, ${project_id}, ${rank});`
 }
 
 export async function test1() {
@@ -28,3 +41,14 @@ export async function test1() {
   await project();
   await evaluate();
 }
+
+// ignore for now - this creates an invalid file
+// test1()
+
+export async function test2() {
+    await reset();
+    const p0 = await project();
+    const u0 = await user(5);
+    await vote(p0, u0, 1);
+    await evaluate();
+  }
