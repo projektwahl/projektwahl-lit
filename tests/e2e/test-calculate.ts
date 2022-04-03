21bb07e9fae30309df24c581b5017046aaa053c6
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-03 22:34:27 +0200

    Initial file is generated

--- tests/e2e/test-calculate.ts
+++ tests/e2e/test-calculate.ts
@@ -1,7 +1,28 @@
+/*
+projektwahl-lit is a software to manage choosing projects and automatically assigning people to projects.
+Copyright (C) 2021 Moritz Hedtke
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see https://www.gnu.org/licenses/.
+*/
+/*!
+https://github.com/projektwahl/projektwahl-lit
+SPDX-License-Identifier: AGPL-3.0-or-later
+SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
+*/
 import { sql } from "../../src/server/database.js";
 import { Chance } from "chance";
 import { evaluate } from "../../src/server/routes/evaluate/index.js";
-import { userInfo } from "os";
 
 const chance = new Chance();
 
@@ -21,19 +42,21 @@ export async function project(
   max_age: number = 13,
   random_assignments: boolean = true
 ) {
-  return (await sql`INSERT INTO projects (title, info, place, costs, min_age, max_age, min_participants, max_participants, random_assignments, last_updated_by) VALUES (${chance.sentence()}, '', '', 0, ${min_age}, ${max_age}, ${min_participants}, ${max_participants}, ${random_assignments}) RETURNING projects.id;`)[0].id;
+  return (
+    await sql`INSERT INTO projects (title, info, place, costs, min_age, max_age, min_participants, max_participants, random_assignments, last_updated_by) VALUES (${chance.sentence()}, '', '', 0, ${min_age}, ${max_age}, ${min_participants}, ${max_participants}, ${random_assignments}, NULL) RETURNING projects.id;`
+  )[0].id;
 }
 
 export async function user(age: number) {
-    return (await sql`INSERT INTO users (username, type, "group", age, last_updated_by) VALUES (${chance.name(
-        { prefix: true, suffix: true }
-      )}, 'voter', '', ${age}, 1) RETURNING users.id;`)[0].id
+  return (
+    await sql`INSERT INTO users (username, type, "group", age, last_updated_by) VALUES (${chance.name(
+      { prefix: true, suffix: true }
+    )}, 'voter', '', ${age}, NULL) RETURNING users.id;`
+  )[0].id;
 }
 
 export async function vote(project_id: number, user_id: number, rank: number) {
-    await sql`INSERT INTO choices (user_id, project_id, rank) VALUES (${
-        user_id
-      }, ${project_id}, ${rank});`
+  await sql`INSERT INTO choices (user_id, project_id, rank) VALUES (${user_id}, ${project_id}, ${rank});`;
 }
 
 export async function test1() {
@@ -46,9 +69,11 @@ export async function test1() {
 // test1()
 
 export async function test2() {
-    await reset();
-    const p0 = await project();
-    const u0 = await user(5);
-    await vote(p0, u0, 1);
-    await evaluate();
-  }
+  await reset();
+  const p0 = await project();
+  const u0 = await user(5);
+  await vote(p0, u0, 1);
+  await evaluate();
+}
+
+await test2();
