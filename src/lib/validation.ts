71c719ad4ead23377f5afd0734bdeab9eb75fd62
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-20 14:30:04 +0200

    More implementation progress

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,41 +1,68 @@
+export type Result<R, E> =
+  | { success: true; data: R }
+  | { success: false; error: E };
 
+export const vnumber = (input: unknown): Result<number, any> => {
+  if (typeof input !== "number") {
+    return {
+      success: false,
+      error: `${input} ist keine Zahl!`,
+    };
+  }
+  return {
+    success: true,
+    data: input,
+  };
+};
 
-export type Result<R, E> = { success: true, data: R } | { success: false, error: E }
-
-export const number = (input: unknown): Result<number, any> => {
-        if (typeof input !== "number") {
-            return {
-                success: false,
-                error: `${input} ist keine Zahl!`
-            }
-        }
-        return {
-            success: true,
-            data: input
-        }
-    }
-
-
-export const object = <K, V>(key: K, value: (input: unknown) => Result<V, any>) => (input: unknown): Result<object, any> => {
+export const vobject =
+  <K extends string | number | symbol, V>(
+    key: K,
+    value: (input: unknown) => Result<V, any>
+  ) =>
+  (input: unknown): Result<{ [k in K]: V }, any> => {
     if (typeof input !== "object") {
-        return {
-            success: false,
-            error: `${input} ist kein Objekt!`
-        }
+      return {
+        success: false,
+        error: `${input} ist kein Objekt!`,
+      };
     }
     if (input === null) {
-        return {
-            success: false,
-            error: `${input} ist null!`
-        }
+      return {
+        success: false,
+        error: `${input} ist null!`,
+      };
     }
     if (!(key in input)) {
-        return {
-            success: false,
-            error: `${input} hat kein Attribut ${key}!`
-        }
+      return {
+        success: false,
+        error: `${input} hat kein Attribut ${key}!`,
+      };
     }
-    if (key in input) {
-        const inner = value(input[key]);
+    // @ts-expect-error probably not typeable
+    const inner = value(input[key]);
+    if (inner.success) {
+      // @ts-expect-error probably not typeable
+      const data: { [k in K]: V } = {
+        [key]: inner.data,
+      };
+      return {
+        success: true,
+        data,
+      };
+    } else {
+      return {
+        success: false,
+        error: {
+          key: `${inner.error}`,
+        },
+      };
     }
-}
+  };
+
+const schema = vobject("helper" as const, vnumber);
+
+console.log(schema({ helper: 1 }));
+console.log(schema({ helper: null }));
+console.log(schema({ he: 1 }));
+console.log(schema({ he: 1, helper: 1 }));
