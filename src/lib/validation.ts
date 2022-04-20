5732ccbd9fde29cfc3e3ecb1346e81bf9ea3ff7a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-04-20 19:01:55 +0200

    Jo

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -3,12 +3,12 @@ export type Result<R, E> =
   | { success: false; error: E };
 
 abstract class VSchema<T> {
+  private schema!: T;
 
-    schema!: T
+  abstract validate(input: unknown): Result<T, any>;
 }
 
-export const vnumber = class VNumber extends VSchema<number> {
-
+export class VNumber extends VSchema<number> {
   validate(input: unknown): Result<number, any> {
     if (typeof input !== "number") {
       return {
@@ -21,66 +21,83 @@ export const vnumber = class VNumber extends VSchema<number> {
       data: input,
     };
   }
-};
-
-export const vobject = class VObject<K extends string | number | symbol, V> extends VSchema<{ [k in K]: V }> {
-  
-  validate(
-    key: K,
-    value: (input: unknown) => Result<V, any>
-  ) {
-  return (input: unknown): Result<{ [k in K]: V }, any> => {
-    if (typeof input !== "object") {
-      return {
-        success: false,
-        error: {
-          // TODO don't stringify as this could explode the error message length
-          [key]: `${JSON.stringify(input)} ist kein Objekt!`,
-        },
-      };
-    }
-    if (input === null) {
-      return {
-        success: false,
-        error: { [key]: `${JSON.stringify(input)} ist null!` },
-      };
-    }
-    if (!(key in input)) {
-      return {
-        success: false,
-        error: { [key]: `${JSON.stringify(input)} hat kein Attribut ${key}!` },
-      };
-    }
-    // @ts-expect-error probably not typeable
-    const inner = value(input[key]);
-    if (inner.success) {
-      // @ts-expect-error probably not typeable
-      const data: { [k in K]: V } = {
-        [key]: inner.data,
-      };
-      return {
-        success: true,
-        data,
-      };
-    } else {
-      return {
-        success: false,
-        error: {
-          key: `${inner.error}`,
-        },
-      };
-    }
 }
-};
 
-export const vintersection =
-  <S1, S2>(
-    schema1: (input: unknown) => Result<S1, any>,
-    schema2: (input: unknown) => Result<S2, any>
-  ) =>
-  (input: unknown): Result<S1 & S2, any> => {
-    const output1 = schema1(input);
-    const output2 = schema2(input);
+export class VObject<K extends string | number | symbol, V> extends VSchema<{
+  [k in K]: V;
+}> {
+    private key: K;
+
+    private value: VSchema<V>
+
+    constructor( key: K, value: VSchema<V>) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+  validate(input: unknown): Result<{ [k in K]: V }, any> {
+      if (typeof input !== "object") {
+        return {
+          success: false,
+          error: {
+            // TODO don't stringify as this could explode the error message length
+            [this.key]: `${JSON.stringify(input)} ist kein Objekt!`,
+          },
+        };
+      }
+      if (input === null) {
+        return {
+          success: false,
+          error: { [this.key]: `${JSON.stringify(input)} ist null!` },
+        };
+      }
+      if (!(this.key in input)) {
+        return {
+          success: false,
+          error: {
+            [this.key]: `${JSON.stringify(input)} hat kein Attribut ${this.key}!`,
+          },
+        };
+      }
+      // @ts-expect-error probably not typeable
+      const inner = value(input[key]);
+      if (inner.success) {
+        // @ts-expect-error probably not typeable
+        const data: { [k in K]: V } = {
+          [this.key]: inner.data,
+        };
+        return {
+          success: true,
+          data,
+        };
+      } else {
+        return {
+          success: false,
+          error: {
+            key: `${inner.error}`,
+          },
+        };
+      }
+    };
+  }
+}
+
+export class VIntersection<S1, S2> extends VSchema<S1 & S2> {
+
+    private schema1: VSchema<S1>
+    private schema2: VSchema<S2>
+
+    constructor(schema1: VSchema<S1>,
+        schema2: VSchema<S2>) {
+        super()
+        this.schema1 = schema1
+        this.schema2 = schema2
+    }
+
+  validate(input: unknown): Result<S1 & S2, any> {
+    const output1 = this.schema1.validate(input);
+    const output2 = this.schema2.validate(input);
     if (!output1.success && !output2.success) {
       return {
         success: false,
@@ -103,7 +120,8 @@ export const vintersection =
         ...output2.data,
       },
     };
-  };
+  }
+}
 
 function setDifference(
   a: (string | number | symbol)[],
@@ -113,13 +131,22 @@ function setDifference(
 }
 
 // https://stackoverflow.com/questions/58779360/typescript-generic-being-incorrectly-inferred-as-unknown ?
-export const vfilterKeys =
-  <S, K extends keyof S>(
-    keys: Set<K>,
-    schema: (input: unknown) => Result<S, any>
-  ) =>
-  (input: unknown): Result<Pick<S, K>, any> => {
-    const diff = setDifference(Object.keys(input), keys);
+export class VFilterKeys<S, K extends keyof S> extends VSchema<Pick<S, K>> {
+
+private keys: Set<K>;
+private parentSchema: VSchema<S>
+  
+    constructor(
+        keys: Set<K>,
+        parentSchema: VSchema<S>) {
+        super();
+            this.keys = keys;
+            this.parentSchema = parentSchema;
+    }
+
+
+  validate(input: unknown): Result<Pick<S, K>, any> {
+    const diff = setDifference(Object.keys(input), this.keys);
     if (diff.size > 0) {
       return {
         success: false,
@@ -128,55 +155,56 @@ export const vfilterKeys =
         ),
       };
     }
-    return schema(input);
-  };
+    return this.parentSchema.validate(input);
+  }
+}
 
-const schema1 = vobject("helper" as const, vnumber);
-const schema2 = vobject("tester" as const, vnumber);
+const schema1 = new VObject("helper" as const, new VNumber());
+const schema2 = new VObject("tester" as const, new VNumber());
 
-console.log(schema1({ helper: 1 }));
-console.log(schema1({ helper: null }));
-console.log(schema1({ he: 1 }));
-console.log(schema1({ he: 1, helper: 1 }));
+console.log(schema1.validate({ helper: 1 }));
+console.log(schema1.validate({ helper: null }));
+console.log(schema1.validate({ he: 1 }));
+console.log(schema1.validate({ he: 1, helper: 1 }));
 
-console.log(schema2({ tester: 1 }));
+console.log(schema2.validate({ tester: 1 }));
 
-const schema = vintersection(schema1, schema2);
+const schema = new VIntersection(schema1, schema2);
 
-console.log(schema({ helper: 1, tester: 1 }));
-console.log(schema({ helper: 1, tejster: 1 }));
-console.log(schema({ hjelper: 1, tester: 1 }));
-console.log(schema({ helliper: 1, testekr: 1 }));
+console.log(schema.validate({ helper: 1, tester: 1 }));
+console.log(schema.validate({ helper: 1, tejster: 1 }));
+console.log(schema.validate({ hjelper: 1, tester: 1 }));
+console.log(schema.validate({ helliper: 1, testekr: 1 }));
 
-const betterSchema = vfilterKeys(
+const betterSchema = new VFilterKeys(
   new Set(["helper", "tester"] as const),
   schema
 );
 
 const testGenericSchema = <K extends string | number | symbol>(k: K) => {
-  const a = vintersection(
-    vobject("helper" as const, vnumber),
-    vobject(k, vnumber)
+  const a = new VIntersection(
+    new VObject("helper" as const, new VNumber()),
+    new VObject(k, new VNumber())
   );
-  return vfilterKeys(new Set(["helper", k] as const), a);
+  return new VFilterKeys(new Set(["helper", k] as const), a);
 };
 
 const testGenericSchema2 = <K extends string | number | symbol>(k: K) => {
-  return vfilterKeys(
+  return new VFilterKeys(
     new Set(["helper"] as const),
-    vintersection(vobject("helper" as const, vnumber), vobject(k, vnumber))
+    new VIntersection(new VObject("helper" as const, new VNumber()), new VObject(k, new VNumber()))
   );
 };
 
 const joGeneric = testGenericSchema("hi");
 
-const parsed = joGeneric("");
+const parsed = joGeneric.validate("");
 
 if (parsed.success) {
   parsed.data.hi;
 }
 
-console.log(betterSchema({ helper: 1, tester: 1 }));
-console.log(betterSchema({ helper: 1, tejster: 1 }));
-console.log(betterSchema({ hjelper: 1, tester: 1 }));
-console.log(betterSchema({ helliper: 1, testekr: 1 }));
+console.log(betterSchema.validate({ helper: 1, tester: 1 }));
+console.log(betterSchema.validate({ helper: 1, tejster: 1 }));
+console.log(betterSchema.validate({ hjelper: 1, tester: 1 }));
+console.log(betterSchema.validate({ helliper: 1, testekr: 1 }));
