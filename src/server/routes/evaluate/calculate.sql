7c0236dd4709d25e373b296f62a810fb8af650bf
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-13 19:09:27 +0200

    Jo

--- src/server/routes/evaluate/calculate.sql
+++ src/server/routes/evaluate/calculate.sql
@@ -27,6 +27,19 @@ SELECT t.user_id, t.project_id, t.rank FROM present_voters, LATERAL
     (WITH c AS (SELECT * FROM choices WHERE choices.user_id = present_voters.id LIMIT 6), c_count AS (SELECT COUNT(*) AS count FROM c)
         SELECT user_id, project_id, rank FROM c WHERE (SELECT count FROM c_count) = 5
         UNION ALL
-        SELECT present_voters.id as user_id, projects.id AS project_id, 0 as rank FROM projects WHERE (SELECT count FROM c_count) != 5 AND present_voters.age >= lower(projects.age_range) AND present_voters.age <= upper(projects.age_range) AND present_voters.project_leader_id IS DISTINCT FROM projects.id
+        SELECT present_voters.id as user_id, projects.id AS project_id, 0 as rank FROM projects WHERE (SELECT count FROM c_count) != 5 AND projects.age_range @> present_voters.age AND present_voters.project_leader_id IS DISTINCT FROM projects.id
     )
     t;
+
+-- CREATE INDEX projects_age_range_idx ON projects_with_deleted USING GIST (age_range);
+
+
+SELECT t.user_id, t.project_id, t.rank FROM present_voters, LATERAL
+    (WITH c AS (SELECT * FROM choices WHERE choices.user_id = present_voters.id LIMIT 6), c_count AS (SELECT COUNT(*) AS count FROM c)
+        SELECT user_id, project_id, rank FROM c WHERE (SELECT count FROM c_count) = 5
+        UNION ALL
+        SELECT present_voters.id as user_id, projects.id AS project_id, 0 as rank FROM projects WHERE (SELECT count FROM c_count) != 5 AND projects.age_range @> present_voters.age AND present_voters.project_leader_id IS DISTINCT FROM projects.id
+    )
+    t;
+
+-- CREATE INDEX projects_age_range_idx ON projects_with_deleted USING GIST (age_range);
