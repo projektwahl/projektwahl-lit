9082769c1e341de68ef224a40f49169d17ab9ebf
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-19 20:55:32 +0200

    Test

--- setup-postgresql.sh
+++ setup-postgresql.sh
@@ -5,3 +5,4 @@ psql -c "CREATE ROLE projektwahl_staging LOGIN PASSWORD 'projektwahl';"
 psql -c "CREATE ROLE projektwahl_staging_admin IN ROLE projektwahl_staging LOGIN PASSWORD 'projektwahl';"
 psql -c "CREATE DATABASE projektwahl_staging OWNER projektwahl_staging_admin;"
 psql --user projektwahl_staging_admin --db projektwahl_staging --single-transaction < /opt/projektwahl-lit/src/server/setup.sql
+psql --user projektwahl_staging_admin --db projektwahl_staging -c "UPDATE settings SET open_date = CURRENT_TIMESTAMP + (- '1 minute'::interval), voting_start_date = CURRENT_TIMESTAMP, voting_end_date = CURRENT_TIMESTAMP + '1 hour', results_date = CURRENT_TIMESTAMP + '2 hours';"
