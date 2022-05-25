42aff583be2c0888dda38ab53097d344e8349988
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-26 00:30:05 +0200

    Update setup-arch-luks2-simple.sh

--- docs/setup-arch-luks2-simple.sh
+++ docs/setup-arch-luks2-simple.sh
@@ -565,6 +565,38 @@ server {
 sudo ln -s /etc/nginx/sites-available/nginx-monitoring.conf /etc/nginx/sites-enabled/
 
 
+
+# https://github.com/prometheus/blackbox_exporter
+sudo pacman -S prometheus-blackbox-exporter
+sudo nano /etc/prometheus/blackbox.yml
+modules:
+
+sudo systemctl enable --now prometheus-blackbox-exporter
+
+sudo nano /etc/prometheus/prometheus.yml
+  - job_name: 'blackbox'
+    metrics_path: /probe
+    params:
+      module: [http_2xx]  # Look for a HTTP 200 response.
+    static_configs:
+      - targets:
+        - http://prometheus.io    # Target to probe with http.
+        - https://prometheus.io   # Target to probe with https.
+        - http://example.com:8080 # Target to probe with http on port 8080.
+    relabel_configs:
+      - source_labels: [__address__]
+        target_label: __param_target
+      - source_labels: [__param_target]
+        target_label: instance
+      - target_label: __address__
+        replacement: 127.0.0.1:9115  # The blackbox exporter's real hostname:port.
+
+
+sudo systemctl reload prometheus
+
+
+
+
 # TODO package with arch / search in aur
 cd /opt
 sudo mkdir nginx-prometheus-exporter
