bb4014e305f1673c58dd79bb1e0b54bc3da32653
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-06-10 19:21:21 +0200

    Fix evaluation sql command

--- src/server/routes/evaluate/calculate.sql
+++ src/server/routes/evaluate/calculate.sql
@@ -1,12 +1,10 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 -- SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 
--- TODO FIXME does this allow voting a first choice 5 times?
-
 SELECT t.user_id, t.project_id, t.rank FROM present_voters, LATERAL
-    (WITH c AS (SELECT * FROM choices WHERE choices.user_id = present_voters.id LIMIT 6), c_count AS (SELECT COUNT(*) AS count FROM c)
-        SELECT user_id, project_id, rank FROM c WHERE (SELECT count FROM c_count) = 5
+    (WITH c AS (SELECT * FROM choices WHERE choices.user_id = present_voters.id), c_valid AS (SELECT COUNT(*) AS count, bit_or(1 << rank) AS ranks FROM c)
+        SELECT user_id, project_id, rank FROM c WHERE (SELECT c_valid.count = 5 AND c_valid.ranks = 62 FROM c_valid)
         UNION ALL
-        SELECT present_voters.id as user_id, projects.id AS project_id, 0 as rank FROM projects WHERE (SELECT count FROM c_count) != 5 AND present_voters.age >= projects.min_age AND present_voters.age <= projects.max_age AND present_voters.project_leader_id IS DISTINCT FROM projects.id
+        SELECT present_voters.id as user_id, projects.id AS project_id, 0 as rank FROM projects WHERE (SELECT NOT (c_valid.count = 5 AND c_valid.ranks = 62) FROM c_valid) AND present_voters.age >= projects.min_age AND present_voters.age <= projects.max_age AND present_voters.project_leader_id IS DISTINCT FROM projects.id
     )
     t WHERE present_voters.force_in_project_id IS NULL ORDER BY t.user_id;
