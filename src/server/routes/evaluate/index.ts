bb2c03e8082bf23c6a3c7ba33c1571024a2dc369
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-07-02 22:16:17 +0200

    Änderung der Gewichtung

--- src/server/routes/evaluate/index.ts
+++ src/server/routes/evaluate/index.ts
@@ -249,11 +249,11 @@ export const rank2points = (rank: number) => {
     case 0:
       return 0;
     case 1:
-      return 11;
+      return 5;
     case 2:
-      return 7;
-    case 3:
       return 4;
+    case 3:
+      return 3;
     case 4:
       return 2;
     case 5:
