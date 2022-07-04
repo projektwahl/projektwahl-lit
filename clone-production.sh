fca62b6f07f3fa7e20ad9fedcc503c3964ee7337
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-07-04 13:50:22 +0200

    Implement force_in_project_id correctly in evaluation

--- clone-production.sh
+++ clone-production.sh
@@ -2,7 +2,10 @@
 
 psql --u postgres --command="DROP DATABASE projektwahl_staging;"
 psql --u postgres --command="CREATE DATABASE projektwahl_staging OWNER projektwahl_staging_admin;"
-psql --username projektwahl_staging_admin --set ON_ERROR_STOP=on projektwahl_staging < ~/Documents/Projektwoche/dumps/dump_2022-07-02\ 21\:49\:30.sql 
+psql --username projektwahl_staging_admin --set ON_ERROR_STOP=on projektwahl_staging < '/home/moritz/Documents/Projektwoche/dumps/dump_2022-07-04 12:16:52.sql'
 psql --username projektwahl_staging_admin --single-transaction --db projektwahl_staging < src/server/setup.sql
 
 NODE_ENV=testing PORT=8443 BASE_URL=https://localhost:8443 CREDENTIALS_DIRECTORY=$PWD DATABASE_HOST=localhost DATABASE_URL=postgres://projektwahl_staging:projektwahl@localhost/projektwahl_staging npm run server
+
+
+DATABASE_URL=postgres://projektwahl_production:projektwahl_production@127.0.0.1/projektwahl_production DATABASE_HOST=/run/postgresql npm run evaluate
--- src/server/routes/evaluate/calculate.sql
+++ src/server/routes/evaluate/calculate.sql
@@ -1,10 +1,16 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 -- SPDX-FileCopyrightText: 2021 Moritz Hedtke <Moritz.Hedtke@t-online.de>
 
-SELECT t.user_id, t.project_id, t.rank FROM present_voters, LATERAL
+-- [ 85, 378, 124, 72, 9, 0 ]
+-- [ 93, 371, 129, 74, 9, 0 ]
+
+SELECT t.user_id as user_id, t.project_id as project_id, t.rank as rank FROM present_voters, LATERAL
     (WITH c AS (SELECT * FROM choices WHERE choices.user_id = present_voters.id), c_valid AS (SELECT COUNT(*) AS count, bit_or(1 << rank) AS ranks FROM c)
         SELECT user_id, project_id, rank FROM c WHERE (SELECT c_valid.count = 5 AND c_valid.ranks = 62 FROM c_valid)
         UNION ALL
         SELECT present_voters.id as user_id, projects.id AS project_id, 0 as rank FROM projects WHERE (SELECT NOT (c_valid.count = 5 AND c_valid.ranks = 62) FROM c_valid) AND projects.random_assignments AND present_voters.age >= projects.min_age AND present_voters.age <= projects.max_age AND present_voters.project_leader_id IS DISTINCT FROM projects.id
     )
-    t WHERE present_voters.force_in_project_id IS NULL ORDER BY t.user_id;
+    t WHERE present_voters.force_in_project_id IS NULL
+UNION ALL
+    SELECT id as user_id, force_in_project_id as project_id, 0 as rank FROM present_voters WHERE present_voters.force_in_project_id IS NOT NULL
+    ORDER BY user_id;
