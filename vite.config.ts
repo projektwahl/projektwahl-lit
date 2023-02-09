22f0253f9ff4877f9b74affe82f59d9565af58d7
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-02-09 23:11:39 +0000

    Seems to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    basicSsl()
+  ],
+  server: {
+    https: true,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:8443',
+        secure: false,
+      },
+    }
+  },
   build: {
-    
     rollupOptions: {
       external: /^lit/,
     },
