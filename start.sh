fa2af83bd258f70352054614fb609e92b3c3fca8
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-03-16 01:19:31 +0100

    maybe fix CI

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-sleep 30 # TODO FIXME wait for postgresql to start up
+sleep 15 # TODO FIXME wait for postgresql to start up
 
 NODE_ENV=development DATABASE_URL=postgres://projektwahl_staging_admin:projektwahl@postgres/projektwahl_staging npm run setup
 PORT=8443 BASE_URL=https://localhost:8443 DATABASE_URL=postgres://projektwahl_staging:projektwahl@postgres/projektwahl_staging CREDENTIALS_DIRECTORY=. npm run server
