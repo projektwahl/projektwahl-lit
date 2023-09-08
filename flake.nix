b9a731d9eb34b81fc7fb3c2daca613dafe7b5d11
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-09-08 22:17:54 +0200

    Add nix stuff

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_latest
+              (pkgs.yarn.override { nodejs = pkgs.nodejs_latest; })
+            ];
+          };
+        }
+      );
+}
