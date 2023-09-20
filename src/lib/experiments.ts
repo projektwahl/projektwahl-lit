739aa1c5a6c994133e8e6b587d8e36f4bc4540da
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-09-20 23:05:40 +0200

    wip

--- src/lib/experiments.ts
+++ src/lib/experiments.ts
@@ -9,14 +9,9 @@ const map = {
       input: "i",
       fun: (input: number) => ["hi", "jo"]
     },
-};
+} as const;
 
-export function mappedTypeMap<MappedType, Key extends (keyof MappedType & keyof Q), Q>(mappedType: MappedType, key: Key, func: (input: MappedType[Key]) => Q[Key]): Q[Key] {
-  return func(mappedType[key])
+const magic = <K extends keyof typeof map>(key: K) => {
+  const ji: { [K in keyof typeof map]: typeof map[K] }[K] = map[key]
+  ji.fun(ji.input)
 }
-
-const someKey: "a" | "b" = "a";
-
-const result = mappedTypeMap<typeof map, keyof typeof map, { [Key in keyof typeof map]: (typeof map)[Key]["input"][] }>(map, someKey, (input) => {
-  return input.fun(input.input)
-})
