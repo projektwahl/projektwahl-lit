e2908421d5d9659ff0e9a49f8a1584f32dda86ea
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-09-22 14:28:45 +0200

    add postcss and purgecss

--- src/lib/experiments.ts
+++
@@ -1,2 +0,0 @@
-// https://stackoverflow.com/questions/76901524/how-to-resolve-this-expression-is-not-callable-on-union-types/76901805#76901805
-// https://github.com/microsoft/TypeScript/pull/47109
